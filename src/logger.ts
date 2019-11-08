2fd64aaecd3c9b921efe9c6e93db695a6f1407f4
Kalle <khirvola@gmail.com>
2019-11-08 14:02:41 +0200

    Add ms to logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,4 @@
-const pad = (t: number) => t.toString().padStart(2, '0');
+const pad = (t: number, length = 2) => t.toString().padStart(length, '0');
 const blue = '\x1b[34m';
 const reset = '\x1b[0m';
 
@@ -7,12 +7,13 @@ export const logger = {
   log: (message?: any, ...optionalParams: any[]) => {
     const now = new Date();
 
-    const dateStr = [now.getDate(), pad(now.getMonth() + 1)].join('.');
-    const timeStr = [
+    const dateStr = [pad(now.getDate()), pad(now.getMonth() + 1)].join('.');
+    const timeStr = `${[
       pad(now.getHours()),
       pad(now.getMinutes()),
       pad(now.getSeconds()),
-    ].join(':');
+    ].join(':')}.${pad(now.getMilliseconds(), 3)}`;
+
     const timestamp = `${blue}${dateStr} ${timeStr}${reset}`;
 
     console.log(timestamp, message, ...optionalParams);
